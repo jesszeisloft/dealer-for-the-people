a19352cec7bdb09987006ef3d2871867d95e57b2
jr-zeisloft <jzeisloft@apple.com>
2021-08-01 19:30:56 -0600

    logging configuation

--- src/main/java/com/jesszeisloft/dealer/project/util/impl/PrinterUtilImpl.java
+++ src/main/java/com/jesszeisloft/dealer/project/util/impl/PrinterUtilImpl.java
@@ -2,16 +2,20 @@ package com.jesszeisloft.dealer.project.util.impl;
 
 import com.jesszeisloft.dealer.project.util.Constants;
 import com.jesszeisloft.dealer.project.util.PrinterUtil;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class PrinterUtilImpl implements PrinterUtil {
+    private final Logger logger = LogManager.getLogger(getClass());
 
     @Override
     public void printReviews(List<String> reviews) {
         IntStream.range(0, Math.min(Constants.NUM_TOP_REVIEWS, reviews.size())).forEach(
-                r -> System.out.println((r + 1) + ". " + reviews.get(r))
+                r -> logger.log(Level.INFO, (r + 1) + ". " + reviews.get(r))
         );
     }
 }
